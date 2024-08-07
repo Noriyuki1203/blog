f7bfa30a16d2bfe08ccda3f82571b826f8e77463
Noriyuki1203 <344.l.fly@gmail.com>
2024-08-08 00:24:07 +0900

    lauyout作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,5 @@
-import { url } from "inspector";
-import Image from "next/image";
-import { title } from "process";
-import React, { Children, ReactNode } from "react";
+import Hero from "@/components/hero";
 
 export default function Home() {
-  const props1 = { title: "タイトル1変数", url: "post.html" };
-  const props2 = { title: "タイトル2変数", url: "post2.html" };
-  const propsCSS = { Children };
-  return (
-    <>
-      <Decoration>
-        <h2>おすすめ記事</h2>
-        <EachPost {...props1} />
-        <EachPost {...props2} />
-      </Decoration>
-    </>
-  );
-}
-
-function Decoration(props: DecorationProps) {
-  return <div style={{ color: "red" }}>{props.children}</div>;
-}
-
-function EachPost(props: EachPostProps) {
-  return (
-    <article>
-      <a href={props.url}>
-        <h3>{props.title}</h3>
-      </a>
-    </article>
-  );
-}
-
-interface EachPostProps {
-  url: string;
-  title: string;
-}
-
-interface DecorationProps {
-  children: ReactNode;
+  return <Hero />;
 }
